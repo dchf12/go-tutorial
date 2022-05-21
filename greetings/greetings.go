a3a7b9bc452c31807d560f84ceaa4634ae01de19
dchf <dchf.jp@gmail.com>
2022-05-21 12:10:59 +0900

    Add handle error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,16 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	s := fmt.Sprintf("Hi,%v. Welcome!", name)
-	return s
+	return s, nil
 }
