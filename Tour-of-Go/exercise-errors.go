44ea9ced6d981d60a71ea855bc7749ca15eb375b
Furuya <dchf.jp@gmail.com>
2022-06-03 19:35:16 +0900

    Create exercise-errors.go

---
+++ Tour-of-Go/exercise-errors.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return x, ErrNegativeSqrt(x)
+	}
+	z := 1.0
+	for {
+		dz := z
+		z -= (z*z - x) / (2 * z)
+		if math.Abs(dz-z) < 1e-10 {
+			break
+		}
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
